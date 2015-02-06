a5dcf51cc9067af208d8efe5bc30c9631fcacd2a
PhilipHuang2 <philiphuang2@gmail.com>
2015-02-06 11:23:55 -0800

    done?

--- OriginalFractal.pde
+++ OriginalFractal.pde
@@ -1,15 +1,40 @@
 void setup(){
-	size(500,500);
+	size(900,600);
 	noLoop();
 
 }
 void draw(){
 	background(255);
+	/*
 	stroke(0);
 	strokeWeight(5);
 	point(width/2, height/2);
 	line(0,height/2,width,height/2);
 	line(2,0,2,height);
 	line(width-3,0,width-3,height);
+	*/
+	loopT(800,50,800);
+	//line(250, 250-15, 250, 250+15);
+ 	//line(250-10, 250-10, 250+10, 250-10);
 }
 
+void loopT(int centerX, int centerY, int tall)
+{
+	//each t is an square of ()
+	if(tall <= 50)
+	{
+		line(centerX, centerY-15, centerX, centerY+15);
+		line(centerX-10, centerY-10, centerX+10, centerY-10);
+
+	}
+	else 
+	{
+	  loopT(centerX, centerY, tall/2);
+	  loopT(centerX-tall/2, centerY, tall/2);
+	  loopT(centerX-tall/2, centerY+tall/3, tall/2);
+	  loopT(centerX-tall/2, centerY+tall/3, tall/2);	
+	}
+	
+
+}
+	
--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -15,20 +15,45 @@ import java.io.IOException;
 public class OriginalFractal extends PApplet {
 
 public void setup(){
-	size(500,500);
+	size(900,600);
 	noLoop();
 
 }
 public void draw(){
 	background(255);
+	/*
 	stroke(0);
 	strokeWeight(5);
 	point(width/2, height/2);
 	line(0,height/2,width,height/2);
 	line(2,0,2,height);
 	line(width-3,0,width-3,height);
+	*/
+	loopT(800,50,800);
+	//line(250, 250-15, 250, 250+15);
+ 	//line(250-10, 250-10, 250+10, 250-10);
 }
 
+public void loopT(int centerX, int centerY, int tall)
+{
+	//each t is an square of ()
+	if(tall <= 50)
+	{
+		line(centerX, centerY-15, centerX, centerY+15);
+		line(centerX-10, centerY-10, centerX+10, centerY-10);
+
+	}
+	else 
+	{
+	  loopT(centerX, centerY, tall/2);
+	  loopT(centerX-tall/2, centerY, tall/2);
+	  loopT(centerX-tall/2, centerY+tall/3, tall/2);
+	  loopT(centerX-tall/2, centerY+tall/3, tall/2);	
+	}
+	
+
+}
+	
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalFractal" };
     if (passedArgs != null) {
